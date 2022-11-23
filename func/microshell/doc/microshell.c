e3b5845b1ff9ed5df3d31b9d6206755b291ca8bb
Clément VIDON <cvidon@student.42.fr>
2022-11-23 18:11:58 +0100

    Update

--- func/microshell/doc/microshell.c
+++ func/microshell/doc/microshell.c
@@ -33,13 +33,13 @@ void	ft_puterr(char *issue, char *cause)
 }
 
 /*
- ** @brief      Count command length in term of tokens
+ ** @brief      Count command size in term of tokens
  **
  ** @param[in]  cmd a shell command
  ** @return     the number of tokens that compose the given command
  */
 
-int	ft_command_len(char **cmd)
+int	ft_command_size(char **cmd)
 {
 	int	count;
 
@@ -56,9 +56,9 @@ int	ft_command_len(char **cmd)
  ** @return     Success of failure.
  */
 
-int	ft_builtin_cd(char **cmd)
+int	ft_cd(char **cmd)
 {
-	if (ft_command_len (cmd) != 2)
+	if (ft_command_size (cmd) != 2)
 	{
 		ft_puterr ("error: cd: bad ", "arguments");
 		return (FAILURE);
@@ -75,12 +75,11 @@ int	ft_builtin_cd(char **cmd)
  ** @brief      Execute a program
  */
 
-int	ft_exec(char **cmd, int cmdlen, char **env, int fd_cpy)
+int	ft_exec(char **cmd, char **env, int fd_cpy)
 {
-	cmd[cmdlen] = NULL;								// Set command end to NULL so we can pass it to execve.
-	dup2 (fd_cpy, STDIN_FILENO);					// Set STDIN_FILENO to fd_cpy
+	dup2 (fd_cpy, STDIN_FILENO);					// Set STDIN to our fd_cpy
 	close (fd_cpy);									// Close fd_cpy
-	if (execve (cmd[0], cmd, env) == -1)			// Execute the program
+	if (execve (cmd[0], cmd, env) == -1)			// Execute
 	{
 		ft_puterr ("error: cannot execute ", cmd[0]);
 		return (FAILURE);
@@ -92,38 +91,38 @@ int	ft_exec(char **cmd, int cmdlen, char **env, int fd_cpy)
  ** @brief      Handle a command with a program
  */
 
-int	ft_program(char **cmd, int cmdlen, char **env, int fd_cpy)
+int	ft_program(char **cmd, int i, char **env, int fd_cpy)
 {
 	int	pid;
 
 	pid = fork ();									// Creates a new process
-	if (pid == 0)									//   [The Child]
+
+	if (pid == 0)									//   [ The Child ]
 	{
-		if (ft_exec (cmd, cmdlen, env, fd_cpy))	// Execute the program
+		cmd[i] = NULL;
+		if (ft_exec (cmd, env, fd_cpy))				// Execute
 			return (FAILURE);
 	}
-	else											//   [The Parent]
+	else											//   [ The Parent ]
 	{
-		close (fd_cpy);								// Close fd_cpy
+		close (fd_cpy);								// Close unused fd_cpy
 		while (wait (NULL) != -1)					// Wait for the child to finish
 			;
-	//fd_cpy = dup (STDIN_FILENO);				// Set fd_cpy to any valid fd
-	}												// XXX useless?
-	//close (fd_cpy);									// Close fd_cpy
+	}
 	return (SUCCESS);
 }
 
 /*
  ** @brief      Handle a command with a pipe
  **
- ** @param[in]  cmd a shell command TODO TOUTE MES COMMANDES (check, might be partout)
- ** @param[in]  cmdlen the shell command number of arguments TODO maybe the index of the command averifier
+ ** @param[in]  cmd a pointer to the current command
+ ** @param[in]  i the command number of tokens
  ** @param[in]  env
  ** @param[i/o] fd_cpy
  ** @return     Success or failure.
  */
 
-int	ft_pipe(char **cmd, int cmdlen, char **env, int *fd_cpy)
+int	ft_pipe(char **cmd, int i, char **env, int *fd_cpy)
 {
 	int	fd[2];
 	int	pid;
@@ -132,10 +131,11 @@ int	ft_pipe(char **cmd, int cmdlen, char **env, int *fd_cpy)
 	pid = fork ();									// Create a new process
 	if (pid == 0)									//   [The Child]
 	{
-		dup2 (fd[1], STDOUT_FILENO);				// Set STDOUT_FILENO to fd[1]
-		close (fd[0]);								// close fd[0]
-		close (fd[1]);								// close fd[1] la sortie de notre pipe() comme on la donne a STDOUT_FILENO
-		if (ft_exec (cmd, cmdlen, env, *fd_cpy))
+		dup2 (fd[1], STDOUT_FILENO);				// Set STDOUT to PIPEOUT
+		close (fd[0]);								// close unused PIPEIN
+		close (fd[1]);								// close PIPEOUT
+		cmd[i] = NULL;
+		if (ft_exec (cmd, env, *fd_cpy))
 			return (FAILURE);
 	}
 	else											//   [The Parent]
@@ -154,11 +154,11 @@ int	ft_pipe(char **cmd, int cmdlen, char **env, int *fd_cpy)
  ** ./microshell /bin/ls "|" /usr/bin/grep microshell ";" /bin/echo hello
  ** ./microshell /bin/pwd ";" cd .. ";" /bin/pwd
  **
- ** - One command per while loop.
- ** - A shell command is a ';', '|' or '\0' terminated string or group of string.
+ ** - One command read per while loop.
+ ** - Each command is ';', '|' or '\0' terminated.
  **
- ** - First command starts right after the executable name
- ** - Next commands start after their preceding  ';' or '|'
+ ** - First command starts right after the executable name.
+ ** - Next commands start right after their preceding  ';' or '|'.
  **
  ** @param[in]  cmd one or multiple commands
  ** @param[in]  env the parent shell environment
@@ -167,29 +167,33 @@ int	ft_pipe(char **cmd, int cmdlen, char **env, int *fd_cpy)
 
 int	main(int ac, char **cmd, char **env)
 {
-	int	cmdlen;
+	int	i;
 	int	fd_cpy;
 
 	(void)ac;
-	cmdlen = 0;
-	fd_cpy = dup (STDIN_FILENO); 					// set fd_cpy to any valid fd
-	while (cmd[cmdlen] && cmd[cmdlen + 1])
+	i = 0;
+	fd_cpy = dup (STDIN_FILENO); // Init fd_cpy (with any valid fd)
+	while (cmd[i] && cmd[i + 1])
 	{
-		cmd += cmdlen + 1;
-		cmdlen = ft_command_len (cmd);
-		if (cmdlen && !strcmp (cmd[0], "cd"))
+		cmd += i + 1;
+
+		i = ft_command_size (cmd);
+
+		if (i && !strcmp (cmd[0], "cd"))
 		{
-			if (ft_builtin_cd (cmd) == FAILURE)
+			if (ft_cd (cmd) == FAILURE)
 				return (FAILURE);
 		}
-		else if (cmdlen && (cmd[cmdlen] == NULL || *cmd[cmdlen] == ';')) // == uniquement si commande unique sans pipe apres !!!!!!!
+		// Last program of a command
+		else if (i && (cmd[i] == NULL || *cmd[i] == ';'))
 		{
-			if (ft_program (cmd, cmdlen, env, fd_cpy) == FAILURE)
+			if (ft_program (cmd, i, env, fd_cpy) == FAILURE)
 				return (FAILURE);
 		}
-		else if (cmdlen && *cmd[cmdlen] == '|') // ex. ls | wc (ls passe ici et wc passe a programme) cat cat ls (cat et cat passse en DEUX fois ici et ls passe au dessus)
+		// Program followed by a one pipe
+		else if (i && *cmd[i] == '|')
 		{
-			if (ft_pipe (cmd, cmdlen, env, &fd_cpy) == FAILURE)
+			if (ft_pipe (cmd, i, env, &fd_cpy) == FAILURE)
 				return (FAILURE);
 		}
 	}
